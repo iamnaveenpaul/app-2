407303dd4e3eca6ca76125569f7a49017fd99d80
Naveen Paul <naveenpaul.markunda@gmail.com>
2019-07-27 18:56:52 +0530

    Init

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', ' <i>' + socket.username + ' joined the chat..</i>');
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', ' <i>' + socket.username + ' left the chat..</i>');
+    })
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+});
+
+const server = http.listen(7000, function() {
+    console.log('listening on *:7000');
+});
